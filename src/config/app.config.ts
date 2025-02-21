1fabd3ddc42048a9b49483ff9d1315ba304a2e79
precious <jkpresh@gmail.com>
2025-02-21 14:20:01 +0100

    chore: added a default message in root url

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/", (req, res) => {
+    res.send("Welcome to Telex IP Geolocator")
+});
 app.get("/integration", integration)
 app.post('/ip-lookup', ipLookup)
 
